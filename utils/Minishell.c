d1e4025a2693de0155d41ad419f28161ec19cb94
mfk-ayoub <ayoubmf514@gmail.com>
2024-08-31 05:45:38 +0100

    from pipi

--- utils/Minishell.c
+++ utils/Minishell.c
@@ -46,10 +46,10 @@ void ft_minishell(char *line,char **env)
   else
     root = creat_tree(list);
   //print_tree(root,0);
-  helper = init_helper(env);
-  find_command(root,helper);
-  free(list);
-  free(helper);
+  // helper = init_helper(env);
+  // find_command(root,helper);
+  // free(list);
+  // free(helper);
   return;
 }
 
