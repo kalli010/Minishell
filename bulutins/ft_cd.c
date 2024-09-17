c9d3b0a8f50562d30e633edca6373fac3a9f4f3b
mfk-ayoub <ayoubmf514@gmail.com>
2024-09-17 23:53:51 +0100

    awadi

--- bulutins/ft_cd.c
+++ bulutins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ayel-mou <ayel-mou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 23:46:23 by ayel-mou          #+#    #+#             */
-/*   Updated: 2024/09/10 13:06:57 by ayel-mou         ###   ########.fr       */
+/*   Updated: 2024/09/14 20:31:55 by ayel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@
 //         return (0);
 //     return (S_ISDIR(dir.st_mode));
 // }
-static void  update_pwd(char *old,char *curr)
-{
-    char  *old_pwd;
-    char *curr_pwd;
+// static void  update_pwd(char *old,char *curr)
+// {
+//     char  *old_pwd;
+//     char *curr_pwd;
 
-    old_pwd = ft_strjoin("OLDPWD=", old);
-    curr_pwd = ft_strjoin("PWD=",curr);
+//     old_pwd = ft_strjoin("OLDPWD=", old);
+//     curr_pwd = ft_strjoin("PWD=",curr);
     
     
-}
+// }
 
 static int	cd_errors(void)
 {
@@ -36,40 +36,43 @@ static int	cd_errors(void)
 	return (EXIT_FAILURE);
 }
 
-int	ft_cd(t_list *list,t_helper *helper)
+int ft_cd(t_list *list, t_helper *helper)
 {
     char *go_path;
     char *current_dir;
-
+    char *new_dir;
+    printf("hello from cd \n");
     current_dir = getcwd(NULL, 0);
-
     if (!list)
         return (EXIT_FAILURE);
     
     if (count_arg(list) != 1 && count_arg(list) != 0)
         return cd_errors();
-    
+
     list = list->next;
-    
-    go_path = NULL;
     if (!list)
-	{	
-		chdir(ft_getenv(helper->envp,"HOME"));
-		return(EXIT_SUCCESS);
-	}
+    {
+        go_path = ft_getenv(helper->envp, "HOME");
+        if (!go_path)
+            return (EXIT_FAILURE); 
+    }
     else
-		go_path = ft_getenv(helper->envp,list->content);
-        
+
+        go_path = ft_strdup(list->content); 
     if (chdir(go_path) != 0)
     {
         write(2, "minishell: cd: ", 15);
-        perror("");
+        perror(go_path); 
         write(2, "\n", 1);
         free(go_path);
+        free(current_dir);
         return (EXIT_FAILURE);
     }
-    update_pwd(current_dir,go_path);
+    new_dir = getcwd(NULL, 0); 
+    // update_pwd(current_dir, new_dir, helper);
+
     free(current_dir);
+    free(new_dir);
     free(go_path);
     return (EXIT_SUCCESS);
 }
--- execute_all/run_execute.c
+++ execute_all/run_execute.c
@@ -6,28 +6,95 @@
 /*   By: ayel-mou <ayel-mou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 16:21:08 by ayel-mou          #+#    #+#             */
-/*   Updated: 2024/09/08 13:20:54 by ayel-mou         ###   ########.fr       */
+/*   Updated: 2024/09/17 23:52:12 by ayel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+int is_only_slashes(t_tree *root)
+{
+    char *str;
+    int i;
 
+    str =  root->content->content;
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] != '/')
+            return (0);
+        i++;
+    }
+    return (1);
+}
 
+int check_root_content(t_tree  *root)
+{
+    if (!ft_strncmp(root->content->content,".",sizeof(".")))
+    {
+        printf("minishell :.: filename argument required\n");
+        printf(".: usage: . filename [arguments]\n");
+        return (2);
+    }
+    if (!ft_strncmp(root->content->content,"!",sizeof("!")))
+    {
+        printf("");
+        return (1);
+    }
+    if (!ft_strncmp(root->content->content,"..",sizeof("..")))
+    {
+        printf("..: command not found\n");
+        return (127);
+    }
+    if (!ft_strncmp(root->content->content, "//", 1) && is_only_slashes(root))
+    {
+        printf("minishell: %s: Is a directory\n", root->content->content);
+        return (126);
+    }
+    return (0);
+}
+
+int flag = 0;
+    
 static int execute_parenthesis(t_tree *root, t_helper *helper)
 {
+    pid_t pid;
+    int status;
+
     if (!root || !root->content)
         return (EXIT_FAILURE);
+    // printf("helooooo\n");
+    pid = fork();
+    if (pid == 0)
+    {
+        if (!!find_command(root, helper))
+            exit(EXIT_FAILURE);
+        exit(EXIT_SUCCESS);
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) 
+            return WEXITSTATUS(status); 
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_FAILURE);
 
-    return (find_command(root, helper));
 }
 
 int	find_command(t_tree *root, t_helper *helper)
 {
+
     if (!root)
         return (EXIT_FAILURE);
-    if (root->content->type == PARENTHESIS)
-        return (execute_parenthesis(root->first_child, helper));
+    g_exit_status = check_root_content(root);
+    if (g_exit_status !=  0)
+        return (g_exit_status);
+    if (root->content->type == AND && flag == 0)
+    {
+        flag = 1;
+        return (execute_parenthesis(root, helper));
+    }
     if (root->content->type == INPUT)
     {
         if (redirect_input(root, helper) != EXIT_SUCCESS)
