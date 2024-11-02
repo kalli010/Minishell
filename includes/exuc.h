ae51b28ceb597983fa4842c5a69247c7f20da117
ayel-mou <ayel-mou@c4r10p6.1337.ma>
2024-11-02 11:05:36 +0100

    update

--- includes/exuc.h
+++ includes/exuc.h
@@ -18,9 +18,7 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 
-# define DEFAULT_PATH \
-	"/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin\
-:/usr/local/sbin:/opt/bin:/opt/sbin"
+
 # define GREEN "\033[1;32m"
 # define RED "\033[1;31m"
 # define YELLOW "\033[1;33m"
